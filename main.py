eb36c3d642e88eab26416eed4a5e5bb314b61182
Olsen <wonghoyyeong@gmail.com>
2025-11-10 02:13:19 +0800

    ignore previous branch

--- main.py
+++ main.py
@@ -1121,6 +1121,7 @@ def user_menu():
             menu_options = [
                 ("Search Books & Check Availability", guest_search_books),
                 ("View Entire Book Catalog", guest_view_catalog),
+                ("Login / Register", home_menu)
             ]
 
 
